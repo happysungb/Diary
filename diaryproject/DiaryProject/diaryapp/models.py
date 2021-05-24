07a8a923c75f83ed9953caeeee09eedd97910472
happysungb <2011440@sookmyung.ac.kr>
2021-05-25 00:24:51 +0900

    회원가입/로그인 기능 추가

--- diaryproject/DiaryProject/diaryapp/models.py
+++ diaryproject/DiaryProject/diaryapp/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class Diary(models.Model):
     title=models.CharField(max_length=200)
     pub_date=models.DateTimeField('data published')
-    Diary=models.TextField()
+    body=models.TextField()
 
     def __str__(self):
         return self.title
 
     def summary(self):
-        return self.Diary[:100]
+        return self.body[:100]
